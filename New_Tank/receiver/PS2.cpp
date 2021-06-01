cb8882112b476c0898e2260626d75c783c12eca3
Nickcan <nick411077@gmail.com>
2021-06-01 23:10:40 +0800

    conform Tank

---
+++ New_Tank/receiver/PS2.cpp
@@ -0,0 +1,87 @@
+#include "PS2.h"
+#include "Arduino.h"
+
+boolean PS2::NewButtonState() {
+  return ((last_buttons ^ buttons) > 0);
+}
+
+/****************************************************************************************/
+boolean PS2::NewButtonState(unsigned int button) {
+  return (((last_buttons ^ buttons) & button) > 0);
+}
+
+/****************************************************************************************/
+boolean PS2::ButtonPressed(unsigned int button) {
+  return(NewButtonState(button) & Button(button));
+}
+
+/****************************************************************************************/
+boolean PS2::ButtonReleased(unsigned int button) {
+  return((NewButtonState(button)) & ((~last_buttons & button) > 0));
+}
+
+/****************************************************************************************/
+boolean PS2::Button(uint16_t button) {
+  return ((~buttons & button) > 0);
+}
+
+/****************************************************************************************/
+byte PS2::Analog(byte button) {
+   return Data[button];
+}
+
+void PS2::DigitalWrite(uint8_t pinNumber, uint8_t status)
+{
+    pinMode(pinNumber, OUTPUT);
+    digitalWrite(pinNumber, status);
+}
+
+void PS2::AnalogWrite(int pinNumber, int LEDChannel, int value)
+{
+    ledcSetup(LEDChannel, 5000, 8);
+    ledcAttachPin(pinNumber, LEDChannel);
+    ledcWrite(LEDChannel, value);
+}
+
+void PS2::read()
+{
+    if (Serial2.available())
+    {
+        for (byte z = 0; z < sizeof(Data); z++)
+        {
+            CheckData[z]=Data[z];
+        }
+        Serial2.readBytes(Data, 8);
+        int Check = Data[0] + Data[1];
+        Serial.println(Check);
+        if (Check == 130)
+        {
+            for (byte z = 0; z < sizeof(Data); z++)
+            {
+                Serial.print(Data[z], HEX);
+            }
+            Serial.println();
+            last_buttons = buttons;
+            buttons =  (uint16_t)(Data[3] << 8) + Data[2];
+            Serial.println(~buttons);
+        }
+        else
+        {
+            Serial.println("BUG");
+            for (byte z = 0; z < sizeof(CheckData); z++)
+            {
+                Data[z]=CheckData[z];
+            }
+            while (Serial2.read() >= 0){}
+        }
+    }
+}
+
+void PS2::write() //PTZ上
+{
+  for (byte z = 0; z < sizeof(sensorData); z++)
+  {
+    Serial2.write(sensorData[z]);
+    Serial.write(sensorData[z]);
+  }
+}
---
+++ New_Tank/receiver/PS2.h
@@ -0,0 +1,54 @@
+#ifndef PS2_h
+    #define PS2_h
+
+#include "Arduino.h"
+
+#define PSB_SELECT      0x0001
+#define PSB_L3          0x0002
+#define PSB_R3          0x0004
+#define PSB_START       0x0008
+#define PSB_PAD_UP      0x0010
+#define PSB_PAD_RIGHT   0x0020
+#define PSB_PAD_DOWN    0x0040
+#define PSB_PAD_LEFT    0x0080
+#define PSB_L2          0x0100
+#define PSB_R2          0x0200
+#define PSB_L1          0x0400
+#define PSB_R1          0x0800
+#define PSB_GREEN       0x1000
+#define PSB_RED         0x2000
+#define PSB_BLUE        0x4000
+#define PSB_PINK        0x8000
+#define PSB_TRIANGLE    0x1000
+#define PSB_CIRCLE      0x2000
+#define PSB_CROSS       0x4000
+#define PSB_SQUARE      0x8000
+
+#define PSS_RX 5
+#define PSS_RY 6
+#define PSS_LX 7
+#define PSS_LY 8
+
+class PS2
+{
+public:
+    void read();
+    void write();
+    void DigitalWrite(uint8_t, uint8_t);
+    void AnalogWrite(int, int, int);
+    boolean Button(uint16_t); //will be TRUE if button is being pressed
+    boolean NewButtonState();
+    boolean NewButtonState(unsigned int); //will be TRUE if button was JUST pressed OR released
+    boolean ButtonPressed(unsigned int);  //will be TRUE if button was JUST pressed
+    boolean ButtonReleased(unsigned int); //will be TRUE if button was JUST released
+    byte Analog(byte);
+
+private:
+    unsigned char Data[8];
+    unsigned char CheckData[8];
+    unsigned char sensorData[8];
+    unsigned int last_buttons;
+    unsigned int buttons;
+};
+
+#endif
