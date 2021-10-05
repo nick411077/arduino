dc0e6155adb1af6d5728eb765d04b00dbabc19bc
Nickcan <nick411077@gmail.com>
2021-10-06 03:03:28 +0800

    ADD: NTTU Tank and homeac

---
+++ NTTU-TankWeb/NTTU-TankWeb.ino
@@ -0,0 +1,197 @@
+#include <WiFi.h>
+#include <SPIFFS.h>
+#include <ESPAsyncWebServer.h>
+
+//雙核運行
+TaskHandle_t Task1;
+
+//WiFi設置
+const char* ssid     = "Lavender";
+const char* password = "12345678";
+
+//WebServer設置
+AsyncWebServer server(80);
+
+// Decode HTTP GET 設置
+String Car = String(5);
+String Pow = String(50); //網站請求的出力變數
+//將 String轉換成int 
+int CarValue = Car.toInt();
+int PowValue = Pow.toInt();
+
+uint8_t status = 0; //為了loop不要重複運行設定狀態變數只運行一次
+
+void setup() {
+  if(!SPIFFS.begin()){
+     Serial.println("An Error has occurred while mounting SPIFFS");
+     return;
+  }
+  Serial.begin(115200);
+  // Connect to Wi-Fi network with SSID and password
+  xTaskCreatePinnedToCore(//雙核運行參數
+             Task1code, /* Task function. */
+             "Task1",   /* name of task. */
+             10000,     /* Stack size of task */
+             NULL,      /* parameter of the task */
+             1,         /* priority of the task */
+             &Task1,    /* Task handle to keep track of created task */
+             0);        /* pin task to core 0 */
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_AP); //WIFI模式
+  WiFi.softAP(ssid, password);
+  IPAddress myIP = WiFi.softAPIP();
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    int args = request->args();
+    Serial.println(args);
+    for (int i = 0; i < args; i++){
+      Serial.print("Param name: ");
+      Serial.println(request->argName(i));
+      Serial.print("Param value: ");
+      Serial.println(request->arg(i));
+      Serial.println("------");
+      if (request->argName(i) == "car")
+      {
+        Car = request->arg(i);
+        CarValue = Car.toInt();//將 String轉換成int
+        status = 1;//更新狀態
+      }
+      else if (request->argName(i) == "pow")//GET網站出力狀態
+      {
+        Pow = request->arg(i);
+        PowValue = Pow.toInt();//將 String轉換成int
+      }
+    }
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+  //server.onRequestBody([](AsyncWebServerRequest *request));
+  server.on("/jquery-3.3.1.min.js", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/jquery-3.3.1.min.js", "text/javascript");
+  });
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
+
+void loop()
+{
+}
+
+void Task1code(void *pvParameters)//雙核運行
+{
+  Serial.print("Task1 running on core ");
+  Serial.println(xPortGetCoreID());
+
+  for (;;)
+  {
+    if (status == 1) //讀取狀態
+    {
+      moto(CarValue,PowValue);
+    }
+    delay(1);
+  }
+}
+
+void DigitalWrite(uint8_t pinNumber, uint8_t status)
+{
+    pinMode(pinNumber, OUTPUT);
+    digitalWrite(pinNumber, status);
+}
+
+void AnalogWrite(int pinNumber, int LEDChannel, int value)
+{
+    ledcSetup(LEDChannel, 15000, 8);
+    ledcAttachPin(pinNumber, LEDChannel);
+    ledcWrite(LEDChannel, value);
+}
+
+void moto(int Value, int Power)
+{
+  byte PW = map(Power,0,100,0,250);
+  byte PW1 = PW-100;
+  byte PW2 = PW;
+  if (PW<=100){PW1=0;}
+  
+  Serial.println(PW);
+  Serial.println(PW1);
+  Serial.println(PW2);
+  switch (Value)
+  { //控制
+  case 1:
+    DigitalWrite(18, HIGH);
+    DigitalWrite(5, LOW);
+    AnalogWrite(25, 1, PW2);
+    DigitalWrite(32, LOW);
+    DigitalWrite(33, HIGH);
+    AnalogWrite(26, 2, PW1);
+    break;
+  case 2:
+    DigitalWrite(18, HIGH);
+    DigitalWrite(5, LOW);
+    AnalogWrite(25, 1, PW2);
+    DigitalWrite(32, LOW);
+    DigitalWrite(33, HIGH);
+    AnalogWrite(26, 2, PW2);
+    break;
+  case 3:
+    DigitalWrite(18, HIGH);
+    DigitalWrite(5, LOW);
+    AnalogWrite(25, 1, PW1);
+    DigitalWrite(32, LOW);
+    DigitalWrite(33, HIGH);
+    AnalogWrite(26, 2, PW2);
+    break;
+  case 4:
+    DigitalWrite(18, HIGH);
+    DigitalWrite(5, LOW);
+    AnalogWrite(25, 1, PW2);
+    DigitalWrite(32, HIGH);
+    DigitalWrite(33, LOW);
+    AnalogWrite(26, 2, PW2);
+    break;
+  case 5:
+    DigitalWrite(18, HIGH);
+    DigitalWrite(5, HIGH);
+    AnalogWrite(25, 1, 0);
+    DigitalWrite(32, HIGH);
+    DigitalWrite(33, HIGH);
+    AnalogWrite(26, 2, 0);
+    break;
+  case 6:
+    DigitalWrite(18, LOW);
+    DigitalWrite(5, HIGH);
+    AnalogWrite(25, 1, PW2);
+    DigitalWrite(32, LOW);
+    DigitalWrite(33, HIGH);
+    AnalogWrite(26, 2, PW2);
+    break;
+  case 7:
+    DigitalWrite(18, LOW);
+    DigitalWrite(5, HIGH);
+    AnalogWrite(25, 1, PW2);
+    DigitalWrite(32, HIGH);
+    DigitalWrite(33, LOW);
+    AnalogWrite(26, 2, PW1);
+    break;
+  case 8:
+    DigitalWrite(18, LOW);
+    DigitalWrite(5, HIGH);
+    AnalogWrite(25, 1, PW2);
+    DigitalWrite(32, HIGH);
+    DigitalWrite(33, LOW);
+    AnalogWrite(26, 2, PW2);
+    break;
+  case 9:
+    DigitalWrite(18, LOW);
+    DigitalWrite(5, HIGH);
+    AnalogWrite(25, 1, PW1);
+    DigitalWrite(32, HIGH);
+    DigitalWrite(33, LOW);
+    AnalogWrite(26, 2, PW2);
+    break;
+  }
+  status = 0;
+}
--- homepowercontroller/homeac/homeac.ino
+++ homepowercontroller/homeac/homeac.ino
@@ -1,6 +1,10 @@
 #include <WiFi.h>
 #include <PZEM004T.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+//雙核運行
+TaskHandle_t Task1;
 
 //PZEM
 HardwareSerial PzemSerial2(2);
@@ -11,12 +15,12 @@ const IPAddress pzemip(192,168,1,1);
 #define MQTT_VERSION MQTT_VERSION_3_1_1
 
 // Wifi: SSID and password
-const char* WIFI_SSID = "IoT office";
-const char* WIFI_PASSWORD = "Iot5195911";
+const char* WIFI_SSID = "罐頭new";
+const char* WIFI_PASSWORD = "nick520301";
 
 // MQTT: ID, server IP, port, username and password
 const PROGMEM char* MQTT_CLIENT_ID = "office_light";
-const PROGMEM char* MQTT_SERVER_IP = "120.106.21.240";
+const PROGMEM char* MQTT_SERVER_IP = "192.168.1.116";
 const PROGMEM uint16_t MQTT_SERVER_PORT = 1883;
 const PROGMEM char* MQTT_USER = "[Redacted]";
 const PROGMEM char* MQTT_PASSWORD = "[Redacted]";
@@ -25,27 +29,25 @@ const PROGMEM char* MQTT_PASSWORD = "[Redacted]";
 const char* MQTT_LIGHT_STATE_TOPIC[3] = {"office/light1/status","office/light2/status","office/light3/status"};
 const char* MQTT_LIGHT_COMMAND_TOPIC[3] = {"office/light1/switch","office/light2/switch","office/light3/switch"};
 
+const char* MQTT_SENSOR_TOPIC[3] = {"office/sensor/switch1","office/sensor/switch2","office/sensor/switch3"};
+
 // payloads by default (on/off)
 const char* LIGHT_ON = "ON";
 const char* LIGHT_OFF = "OFF";
-
+const PROGMEM uint8_t PZEM_PIN[3] = {12,14,27};
 const PROGMEM uint8_t LED_PIN[3] = {5,18,19};
+
 boolean m_light_state[3] = {false,false,false}; // light is turned off by default
 
-float cur =0;      //電流
-float vol=0;      //電壓
-float powe;     //功率
-float pe;
-float pw=0;
+unsigned int delaytime = 10000;
+float cur = 5.84;      //電流
+float vol = 120.58;      //電壓
+float powe = 50.5;     //功率
+float powehr = 40.72;
 
-WiFiClient espClient;
-PubSubClient client(espClient);
 
-void DigitalWrite(int pinNumber, boolean status)
-{
-    pinMode(pinNumber, OUTPUT);
-    digitalWrite(pinNumber, status);
-}
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
 
 void setup_wifi() {
 
@@ -71,6 +73,32 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
+void publishData(uint8_t i, float p_voltage, float p_current, float p_power, float p_energy) {
+  // create a JSON object
+  // doc : https://github.com/bblanchon/ArduinoJson/wiki/API%20Reference
+  StaticJsonDocument<200> jsonBuffer;
+  JsonObject root = jsonBuffer.to<JsonObject>();
+  // INFO: the data must be converted into a string; a problem occurs when using floats...
+  root["voltage"] = (String)p_voltage;
+  root["current"] = (String)p_current;
+  root["power"] = (String)p_power;
+  root["energy"] = (String)p_energy;
+  serializeJsonPretty(root, Serial);
+  Serial.println("");
+  /*
+     {
+        "voltage": "123.20",
+        "current": "3.70",
+        "power": "5.60",
+        "energy": "4.47"
+     }
+  */
+  char data[200];
+  serializeJson(root, data, measureJson(root) + 1);
+  client.publish(MQTT_SENSOR_TOPIC[i], data, true);
+  yield();
+}
+
 void publishLightState(uint8_t i) {
   if (m_light_state[i]) {
     client.publish(MQTT_LIGHT_STATE_TOPIC[i], LIGHT_ON, true);
@@ -83,10 +111,10 @@ void publishLightState(uint8_t i) {
 void setLightState(uint8_t i) {
   if (m_light_state[i]) {
     digitalWrite(LED_PIN[i], HIGH);
-    Serial.println("INFO: Turn light on...");
+    Serial.println("INFO: Turn Switch" + String(i+1) + " on...");
   } else {
     digitalWrite(LED_PIN[i], LOW);
-    Serial.println("INFO: Turn light off...");
+    Serial.println("INFO: Turn Switch" + String(i+1) + " off...");
   }
 }
 
@@ -138,7 +166,7 @@ void reconnect() {
       // ... and resubscribe
       for (size_t i = 0; i < 3; i++)
       {
-        publishLightState(i);
+        //publishLightState(i); //發布者有啟用保留可以在重起時讀取舊數據可以不用預設關閉
         client.subscribe(MQTT_LIGHT_COMMAND_TOPIC[i]);
       }
     } else {
@@ -154,17 +182,26 @@ void reconnect() {
 void setup()
 {
   Serial.begin(115200);
+  Serial2.begin(115200);
   setup_wifi();
   client.setServer(MQTT_SERVER_IP, MQTT_SERVER_PORT);
   client.setCallback(callback);
+  Serial.print("Connecting to PZEM...");
   while (true)
   {
-    Serial.print("Connecting to PZEM...");
     if (pzem.setAddress(pzemip))
       break;
     Serial.print(".");
     delay(500);
   }
+  xTaskCreatePinnedToCore(//雙核運行參數
+             Task1code, /* Task function. */
+             "Task1",   /* name of task. */
+             10000,     /* Stack size of task */
+             NULL,      /* parameter of the task */
+             1,         /* priority of the task */
+             &Task1,    /* Task handle to keep track of created task */
+             0);        /* pin task to core 0 */
 }
 
 
@@ -176,18 +213,47 @@ void loop()
   client.loop();
 }
 
-void getdata()
+void getdata(uint8_t i)
 {
-  cur = pzem.current(pzemip);
-  //<<0609test
+  //cur = pzem.current(pzemip);
   Serial.print("Pzem Current(A): ");
   Serial.println(cur);
-  //0609test>> 
-       
-  vol = pzem.voltage(pzemip);
-//<<20200422
+  //vol = pzem.voltage(pzemip);
+  Serial.print("Pzem Current(V): ");
+  Serial.println(vol);
+  //powe = pzem.power(pzemip);
+  Serial.print("Pzem Current(W): ");
+  Serial.println(powe);
+  //powehr = pzem.energy(pzemip);
+  powehr = random(300);
+  Serial.print("Pzem Current(W): ");
+  Serial.println(powehr);
   if (vol < 0) vol = 0;
   if (cur < 0) cur = 0;
-  if (pw < 0) pw = 0;
+  if (powe < 0) powe = 0;
+  if (powehr < 0) powehr = 0;
+  publishData(i, vol, cur, powe, powehr);
+}
 
+void EnergySwitch(uint8_t i, unsigned int TIME)
+{
+  digitalWrite(PZEM_PIN[i], HIGH);
+  getdata(i);
+  delay(TIME);
+  digitalWrite(PZEM_PIN[i], LOW);
+}
+
+void Task1code(void *pvParameters)//雙核運行
+{
+  Serial.print("Task1 running on core ");
+  Serial.println(xPortGetCoreID());
+
+  for (;;)
+  {
+    for (size_t i = 0; i < 3; i++)
+    {
+      EnergySwitch(i, delaytime);
+    }
+    delay(1);
+  }
 }
